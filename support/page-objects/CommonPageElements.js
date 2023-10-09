e70503cef11ce7ef903ba6147105761164e8bf38
Andres Palavicini <apalavicini@its4logistics.com>
2023-10-09 11:41:15 -0600

    code Refactor

--- support/page-objects/CommonPageElements.js
+++ support/page-objects/CommonPageElements.js
@@ -5,9 +5,14 @@ exports.CommonPageElements = class CommonPageElements {
     constructor(page) {
         this.page= page;
         this.switchBackgroundColor = page.locator('span.s__sbzCIpCki6YQQPcAH6cb');
-        this.fromInputField = page.locator('input#origin.autocomplete__input');
-        this.toInputField = page.locator('input#destination.autocomplete__input');
+        this.originField = page.locator('input#origin.autocomplete__input');
+        this.destinationField = page.locator('input#destination.autocomplete__input');
+        this.departureDateField = page.locator(' [data-test-id="departure-date-input"]');
+        this.returnDateField = page.locator('[data-test-id="return-date-input"]');
+        this.passengerAndClassLabel = page.locator('div.avia-form__field.--passengers > div > div > div.additional-fields__label');
+        this.autocompleteSuggestion = page.locator ('.autocomplete__suggestion-info');
         this.searchFlightsButton = page.locator('button[type="submit"]');
+        this.cheapestLabel = page.locator('div:nth-child(3) > div > div > strong > span[data-test-id="text"]:nth-child(1)');
     }
 
     async clickSwitchBackgroundButton() {
@@ -17,5 +22,15 @@ exports.CommonPageElements = class CommonPageElements {
     async clickSearchFlightsButton() {
         await this.searchFlightsButton.click();
     }
-    
+
+    async checkNightBackgroundIsEnabled(isEnabled) {
+        const mainPage = await this.page.locator('html');
+        const attributeValue = await mainPage.getAttribute('class');
+
+        if (isEnabled) {
+            expect(attributeValue).toContain('--night');
+        } else {
+            expect(attributeValue).not.toContain('--night');
+        }
+    }
 };
--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,53 +1,35 @@
-// @ts-check
-const { test } = require('@playwright/test');
-const { HomePage } = require('../support/page-objects/HomePage');
-const { CommonPageElements } = require('../support/page-objects/CommonPageElements');
-// @ts-ignore
-const { SearchPage } = require('../support/page-objects/SearchPage');
+  // @ts-check
+  const { test } = require('@playwright/test');
+  const { HomePage } = require('../support/page-objects/HomePage');
+  const { CommonPageElements } = require('../support/page-objects/CommonPageElements');
+  // @ts-ignore
+  const { SearchPage } = require('../support/page-objects/SearchPage');
+ 
 
-test.describe('Test the Aviasales ticket search functionality', () => {
+  test.describe('Test the Aviasales ticket search functionality', () => {
 
-  test('Verify the user is able to do a ticket search', async ({ page }) => {
-    const homePage = new HomePage(page);
-    const searchPage = new SearchPage(page);
-    const commonPageElements = new CommonPageElements(page);
-    await homePage.goToAviasalesHomePage();
-    await commonPageElements.clickSwitchBackgroundButton();
-    await homePage.clickOpenBookingInANewTabCheckbox();
-    await homePage.checkNightBackgroundIsEnabled(true);
-    await homePage.enterFromField('John F. Kennedy International Airport');
-    await homePage.enterToField('Berlin');
-    await homePage.selectCalendarDate('2023-10',"30");
-    await homePage.selectPassengerTypeAndAddPassengers('Adults', 2);
-    await commonPageElements.clickSearchFlightsButton();
-    await searchPage.checkNewSearchPageisOpen();
-    await searchPage.checkValuesOnFilterForm(
-      "John F. Kennedy International Airport",
-      "Berlin",
-      'Mon, October 30',
-      null,
-      '2 passengerseconomy'
-    );
-  })
-
-  test.skip('Verify the Aviasales is searching the cheapest flight ticket', async ({ page }) => {
-    const homePage = new HomePage(page);
-    const searchPage = new SearchPage(page);
-    const commonPageElements = new CommonPageElements(page);
-    await homePage.goToAviasalesHomePage();
-    await homePage.clickOpenBookingInANewTabCheckbox();
-    await homePage.checkNightBackgroundIsEnabled(false);
-    await homePage.enterFromField('John F. Kennedy International Airport');
-    await homePage.enterToField('Berlin');
-    await homePage.selectCalendarDate('2023-11', "30");
-    await homePage.clickIDontNeedReturnTicketButton()
-    await homePage.selectPassengerTypeAndAddPassengers('Adults', 2);
-    await homePage.selectPassengerClass('Economy');
-    await commonPageElements.clickSearchFlightsButton();
-    await searchPage.checkNewSearchPageisOpen();
-    await searchPage.displayAllTickets();
-  })
-
-
-
-});
+    test('Verify the user is able to do a ticket search', async ({ page }) => {
+      const homePage = new HomePage(page);
+      const searchPage = new SearchPage(page);
+      const commonPageElements = new CommonPageElements(page);
+      await homePage.goToAviasalesHomePage();
+      await commonPageElements.clickSwitchBackgroundButton();
+      await homePage.clickOpenBookingInANewTabCheckbox();
+      await commonPageElements.checkNightBackgroundIsEnabled(true);
+      await homePage.fillOriginField('Kennedy int');
+      await homePage.fillDestinationField('Berlin');
+      await homePage.selectCalendarDate('November', "30");
+      await homePage.clickIDontNeedReturnTicketButton();
+      await homePage.selectPassengerTypeAndAddPassengers('Adults', 2);
+      await commonPageElements.clickSearchFlightsButton();
+      await searchPage.checkNewSearchPageisOpen();
+      await commonPageElements.checkNightBackgroundIsEnabled(true);
+      await searchPage.checkValuesOnFilterForm(
+        "John F. Kennedy International Airport",
+        "Berlin",
+        'Mon, October 30',
+        null,
+        '2 passengers economy'
+      );
+    });
+  });
